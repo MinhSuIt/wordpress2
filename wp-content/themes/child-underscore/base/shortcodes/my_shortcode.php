c31e96a99f0d31a5a92111e09ef557b336816b39
minhsudoit <minhsudoit@gmail.com>
2026-01-27 23:07:32 +0700

    s

--- wp-content/themes/child-underscore/base/shortcodes/my_shortcode.php
+++ wp-content/themes/child-underscore/base/shortcodes/my_shortcode.php
@@ -1,20 +1,15 @@
 <?php
 function my_shortcode($atts, $content = null)
 {
-    wp_enqueue_style(
-        'my-css',
-        get_stylesheet_directory_uri() . '/base/template-parts/my_shortcode/style.css',
-    );
+    wp_enqueue_style('my-css', get_stylesheet_directory_uri() . '/base/template-parts/my_shortcode/style.css');
+    wp_enqueue_script('my-js', get_stylesheet_directory_uri() . '/base/template-parts/my_shortcode/js.js', [], '1.0', ['strategy' => 'defer']);
 
-    wp_enqueue_script(
-        'my-js',
-        get_stylesheet_directory_uri() . '/base/template-parts/my_shortcode/js.js',
-        [],
-        '1.0',
-        ['strategy' => 'defer']
-    );
-
-    return '<div class="my-box">' . do_shortcode($content) . '</div>';
+    ob_start(); ?>
+    <div class="my-box">
+        <?php echo do_shortcode($content); ?>
+    </div>
+<?php
+    return ob_get_clean();
 }
 add_shortcode('my_shortcode', 'my_shortcode');
 
